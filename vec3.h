b70655693f9ec3d74c5ea9596d673bf82ae0fefe
Mark Liu <markliu202@gmail.com>
2024-01-13 13:25:55 -0500

    vec3 color:

--- vec3.h
+++ vec3.h
@@ -1,59 +1,62 @@
 #ifndef VEC3_H
 #define VEC3_H
 
-// recall: include gauards: prevents double declaration of any identifiers such as types, enums and static variables. also prevents recursive inclusions
-
 #include <cmath>
 #include <iostream>
 
 using std::sqrt;
 
 class vec3 {
-    public:
-        double e[3];
+  public:
+    double e[3];
 
-        vec3() : e{0, 0, 0} {}
-        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+    vec3() : e{0,0,0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-        double x() const { return e[0]; }
-        double y() const { return e[1]; }
-        double z() const { return e[2]; }
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
 
-        vec3 operator-() const {return vec3(-e[0], -e[1], -e[2]); }
-        double operator[](int i) const { return e[i]; }
-        double& operator[](int i) {return e[i]; }
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double& operator[](int i) { return e[i]; }
 
-        vec3& operator+=(const vec3 &v) {
-            e[0] += v.e[0];
-            e[1] += v.e[1];
-            e[2] += v.e[2];
-            return *this;
-        }
+    vec3& operator+=(const vec3 &v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
 
-        vec3& operator*=(double t) {
-            e[0] *= t;
-            e[1] *= t;
-            e[2] *= t;
-            return *this;
-        }
+    vec3& operator*=(double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
 
-        vec3& operator/=(double t) {
-            return *this *= 1/t;
-        }
+    vec3& operator/=(double t) {
+        return *this *= 1/t;
+    }
 
-        double length() const {
-            return sqrt(length_squared());
-        }
+    double length() const {
+        return sqrt(length_squared());
+    }
 
-        double length_squared() const {
-            return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
-        }        
+    double length_squared() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
 };
 
 // point3 is just an alias for vec3, but useful for geometric clarity in the code.
 using point3 = vec3;
 
-// Vector utility functions
+
+// Vector Utility Functions
+
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
 
 inline vec3 operator+(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
@@ -79,7 +82,20 @@ inline vec3 operator/(vec3 v, double t) {
     return (1/t) * v;
 }
 
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
+}
 
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
 
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
 
 #endif
